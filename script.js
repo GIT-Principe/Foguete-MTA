b1c21e44f1bd3bf8bf469df5a4df8768f881a324
Principe <80657307+GIT-Principe@users.noreply.github.com>
2026-02-18 00:30:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function loadServer(){
     try{
-        const response = await fetch("https://nonascendantly-uncertain-marcellus.ngrok-free.dev");
+        const response = await fetch("https://nonascendantly-uncertain-marcellus.ngrok-free.dev/players");
         const data = await response.json();
 
         document.getElementById("serverName").innerText = data.name;
@@ -134,6 +134,7 @@ async function loadServer(){
 
 loadServer();
 setInterval(loadServer, 10000);
+
 
 
 
